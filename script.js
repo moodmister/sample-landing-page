1a38db87c5af0a08ea02c9661586bf7dcfa5d1e3
moodmister <v.stoyanov0069@gmail.com>
2022-08-01 11:21:52 +0300

    Center image in slideshow

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(() => {
   var slideIndex = 0;
   const showSlides = () => {
     slides.css('display', 'none');
-    slides[slideIndex].style.display = 'block';
+    slides[slideIndex].style.display = 'flex';
     slideIndex++;
     if (slideIndex > slides.length - 1)
       slideIndex = 0;
